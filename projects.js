d2614183202494f76ae4cac9463077d795c14fec
Sherwin Degay Latawan <131456650+userweyn@users.noreply.github.com>
2024-07-01 16:19:27 +0800

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,38 @@
+const projects=[
+    {
+        name: "SHOPEE",
+        description: "First practice project created solely with HTML and CSS. One page copy of Shopee website created with HTML and CSS only. Sign up and Log in button redirect you to the official Shopee website. Close the tab to continue exploring the project.",
+        image: "images/thumbnails/shopee.PNG",
+        location: "projects/shopee/shopee.html"
+    },
+    {
+        name: "NETFLIX",
+        description: "Two screenshot copies of Netflix webpage made with HTML and CSS, click the logo to alternate between pages. Sign up button redirect you to the official Netflix website. Close the tab to continue exploring the project.",
+        image: "images/thumbnails/netflix.PNG",
+        location: "projects/netflix/netflix.html"
+    },
+    {
+        name: "CALCULATOR",
+        description: "A simple calculator to practice and figure out how to put value in an input tag when a button is clicked, get the value and use that value to perform a task, how to edit it and display the result. .value property and eval method is used.",
+        image: "images/thumbnails/calculator.PNG",
+        location: "projects/calculator/calculator.html"
+    },
+    {
+        name: "IT0043 (School Project)",
+        description: "Redesign copy of shopee. Served as my third practice project using HTML and CSS for the design and structure and JavaScript to add products. Still figuring out the logic on how to get a certain value of an object inside an array. Foreach loop is used here but sadly, I don't really get it. .innerhtml is an important DOM manipulation property I learned in this project.",
+        image: "images/thumbnails/it0043.PNG",
+        location: "projects/school-project/it0043/index.html"
+    },
+    {
+        name: "IT0051 (School Project)",
+        description: "A mockup design for an appartment listing website named UPA. No other features, just plain HTML and CSS",
+        image: "images/thumbnails/it0051.JPG",
+        location: "projects/school-project/it0051/login.html"
+    },
+    {
+        name: "IT0063 (School Project)",
+        description: "Utilizing mainly HTML and CSS with a touch of Javascript",
+        image: "images/thumbnails/it0063.JPG",
+        location: "projects/school-project/it0063/index.html"
+    }
+];
